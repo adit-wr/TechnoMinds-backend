87962999efa35194b6d4c63fccb54d4788e4a1c2
adit-wr <ganding0002nd@gmail.com>
2024-12-04 13:06:44 +0700

    deploy

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const adminAuthorization = require('./middleware/adminAuthorization')
-dotenv.config()
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const adminAuthorization = require('./middleware/adminAuthorization');
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 app.get('/', (res, req) => {
     console.log(tes)
     res.send('helo world')
-})
-const authController = require('./auth/auth.controller')
-const materialController = require('./material/material.controller')
-const spkController = require('./spk/spk.controller')
-const userController = require('./user/user.controller')
+});
+const authController = require('./auth/auth.controller');
+const materialController = require('./material/material.controller');
+const spkController = require('./spk/spk.controller');
+const userController = require('./user/user.controller');
 
-app.use('/api/auth', authController)
-app.use('/api/materials', materialController)
-app.use('/api/spk', spkController)
-app.use('/api/user', adminAuthorization, userController)
+app.use('/api/auth', authController);
+app.use('/api/materials', materialController);
+app.use('/api/spk', spkController);
+app.use('/api/user', adminAuthorization, userController);
 
 export default app;
