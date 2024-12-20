7d3583de2ae7e8565ee422e92292203854a7ea09
akbarzn <akbarzn13@gmail.com>
2024-12-20 22:19:46 +0700

    update database

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const PORT = process.env.PORT ;
-dotenv.config();
-
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT
+const adminAuthorization = require('./middleware/adminAuthorization')
 const cors = require('cors')
 
+app.use(express.json())
 app.use(cors())
-
-
-const adminAuthorization = require('./middleware/adminAuthorization');
-app.use('/uploads', express.static('uploads'))
-app.use(express.json());
 app.get('/', (res, req) => {
     console.log(tes)
     res.send('helo world')
-});
-const authController = require('./auth/auth.controller');
-const materialController = require('./material/material.controller');
-const spkController = require('./spk/spk.controller');
-const userController = require('./user/user.controller');
+})
+const authController = require('./auth/auth.controller')
+const materialController = require('./material/material.controller')
+const spkController = require('./spk/spk.controller')
+const userController = require('./user/user.controller')
 
-app.use('/api/auth', authController);
-app.use('/api/materials', materialController);
-app.use('/api/spk', spkController);
-app.use('/api/user', adminAuthorization, userController);
+app.use('/api/auth', authController)
+app.use('/api/materials', materialController)
+app.use('/api/spk', spkController)
+app.use('/api/user', adminAuthorization, userController)
 
 // export default app;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log('server berjalan port : ' + PORT)
+})
